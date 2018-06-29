626bb7768e22004d7251845fc8d52bf84a64b4be
relics9 <relics9@relics9-linux>
2018-06-29 23:23:15 +0900

    test

--- src/main/java/io/hexaforce/songs/SongListController.java
+++ src/main/java/io/hexaforce/songs/SongListController.java
@@ -1,8 +1,19 @@
 package io.hexaforce.songs;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SongListController {
-
+	
+    @GetMapping("/song-list.html")
+    public String showVetList(Map<String, Object> model) {
+//        Vets vets = new Vets();
+//        vets.getVetList().addAll(this.vets.findAll());
+//        model.put("vets", vets);
+        return "vets/vetList";
+    }
+    
 }
