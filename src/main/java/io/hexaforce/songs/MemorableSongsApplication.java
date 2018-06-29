3286b7706e3ea317c8c97645b8dfb0678042fa32
Tantaka Tomokazu <tomo-tantaka@ixit.co.jp>
2018-06-29 14:59:11 +0900

    initial commit

---
+++ src/main/java/io/hexaforce/songs/MemorableSongsApplication.java
@@ -0,0 +1,13 @@
+package io.hexaforce.songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorableSongsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MemorableSongsApplication.class, args);
+	}
+	
+}
