7e28b21ea31af42a6046c79d5cb4041007682187
Tantaka Tomokazu <tomo-tantaka@ixit.co.jp>
2018-07-02 11:34:21 +0900

    test

--- src/main/java/io/hexaforce/songs/common/SongListUtile.java
+++ src/main/java/io/hexaforce/songs/common/SongListUtile.java
@@ -1,8 +1,18 @@
 package io.hexaforce.songs.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import io.hexaforce.songs.MemorableSongsProperties;
 
 @Component
 public class SongListUtile {
 
+	@Autowired
+	private MemorableSongsProperties memorableSongsProperties;
+	
+	public void aaa() {
+		memorableSongsProperties.getSongDirectory();
+	}
+	
 }
