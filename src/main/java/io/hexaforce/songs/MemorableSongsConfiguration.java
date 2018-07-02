338b67431bb83f37e0bc9b27665e388163bbf817
Tantaka Tomokazu <tomo-tantaka@ixit.co.jp>
2018-07-02 15:26:56 +0900

    test

--- src/main/java/io/hexaforce/songs/MemorableSongsConfiguration.java
+++ src/main/java/io/hexaforce/songs/MemorableSongsConfiguration.java
@@ -2,67 +2,49 @@ package io.hexaforce.songs;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.farng.mp3.MP3File;
 import org.farng.mp3.TagException;
-import org.farng.mp3.id3.ID3v1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.extern.slf4j.Slf4j;
+import io.hexaforce.songs.model.MusicItem;
+import io.hexaforce.songs.model.MusicItemRepository;
 
-@Slf4j
 @Configuration
 @EnableConfigurationProperties(MemorableSongsProperties.class)
 public class MemorableSongsConfiguration implements WebMvcConfigurer {
 
 	@Autowired
 	private MemorableSongsProperties memorableSongsProperties;
-
+	
+	@Autowired
+	private MusicItemRepository musicItemRepository;
+	
 	@PostConstruct
 	private void setupSongListDatabase() throws IOException, TagException {
-
+		
+		List<MusicItem> musicItemList = new ArrayList<MusicItem>();
 		File topMusicDirectory = new File(memorableSongsProperties.getTopMusicDirectory());
 		for (File musicDirectory : topMusicDirectory.listFiles()) {
 			for (File musicFile : musicDirectory.listFiles()) {
-				
 				MP3File mp3File = new MP3File(musicFile);
-				log.debug("mp3File:{}", mp3File);
-
-				if (mp3File.hasFilenameTag()) {
-					log.debug("FilenameTag:{}", mp3File.getFilenameTag());
-				}
-
-				if (mp3File.hasID3v1Tag()) {
-					log.debug("ID3v1Tag:{}", mp3File.getID3v1Tag());
-					ID3v1 tag = mp3File.getID3v1Tag();
-					
-					tag.getGenre();
-					
-					tag.getArtist();
-					tag.getSongTitle();
-					
-					tag.getAlbumTitle();
-					tag.getTrackNumberOnAlbum();
-					
-					tag.getYearReleased();
-					
-
-				}
-
 				if (mp3File.hasID3v2Tag()) {
-					log.debug("ID3v2Tag:{}", mp3File.getID3v2Tag());
+					musicItemList.add(new MusicItem(mp3File.getID3v2Tag()));
+				} else if (mp3File.hasID3v1Tag()) {
+					musicItemList.add(new MusicItem(mp3File.getID3v1Tag()));
 				}
-
-				if (mp3File.hasLyrics3Tag()) {
-					log.debug("Lyrics3Tag:{}", mp3File.getLyrics3Tag());
-				}
-				
 			}
+		}
+		
+		if (!musicItemList.isEmpty()) {
+			musicItemRepository.saveAll(musicItemList);
 		}
 		
 	}
--- src/main/java/io/hexaforce/songs/model/MusicItem.java
+++ src/main/java/io/hexaforce/songs/model/MusicItem.java
@@ -1,16 +1,58 @@
 package io.hexaforce.songs.model;
 
+import java.io.UnsupportedEncodingException;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import org.farng.mp3.id3.AbstractID3v2;
+import org.farng.mp3.id3.ID3v1;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "music_item")
 public class MusicItem extends BaseEntity {
-	private String songTitle;
-	private String albumTitle;
-	private String track;
+
+	private String album;
 	private String artist;
+	private String comment;
+	private String title;
+	private String year;
+	//private String genre;
+
+	public MusicItem(ID3v1 tag) {
+		this.album = convert(tag.getAlbumTitle());
+		this.artist = convert(tag.getArtist());
+		this.comment = convert(tag.getComment());
+		this.title = convert(tag.getTitle());
+		this.year = convert(tag.getYear());
+		//this.genre = Byte.toString(tag.getGenre());
+	}
+
+	public MusicItem(AbstractID3v2 tag) {
+		this.album = tag.getAlbumTitle();
+		this.artist = tag.getLeadArtist();
+		this.comment = tag.getSongComment();
+		this.title = tag.getSongTitle();
+		this.year = tag.getYearReleased();
+		//this.genre = tag.getSongGenre();
+	}
+	
+	private String convert (String x) {
+		try {
+			return new String (x.getBytes("ISO-8859-1"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return x;
+	}
+	
 }
