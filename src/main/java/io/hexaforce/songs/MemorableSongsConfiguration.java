ba11746a21f05e424f1251682919d1a1101dcb1c
Tantaka Tomokazu <tomo-tantaka@ixit.co.jp>
2018-07-02 13:22:14 +0900

    test

--- src/main/java/io/hexaforce/songs/MemorableSongsConfiguration.java
+++ src/main/java/io/hexaforce/songs/MemorableSongsConfiguration.java
@@ -1,9 +1,13 @@
 package io.hexaforce.songs;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+import org.farng.mp3.id3.ID3v1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +24,44 @@ public class MemorableSongsConfiguration implements WebMvcConfigurer {
 	private MemorableSongsProperties memorableSongsProperties;
 
 	@PostConstruct
-	private void setupSongListDatabase() {
+	private void setupSongListDatabase() throws IOException, TagException {
 
 		File topMusicDirectory = new File(memorableSongsProperties.getTopMusicDirectory());
 		for (File musicDirectory : topMusicDirectory.listFiles()) {
-			for (File musicItem : musicDirectory.listFiles()) {
-				log.debug("musicItem:{}", musicItem);
+			for (File musicFile : musicDirectory.listFiles()) {
+				
+				MP3File mp3File = new MP3File(musicFile);
+				log.debug("mp3File:{}", mp3File);
+
+				if (mp3File.hasFilenameTag()) {
+					log.debug("FilenameTag:{}", mp3File.getFilenameTag());
+				}
+
+				if (mp3File.hasID3v1Tag()) {
+					log.debug("ID3v1Tag:{}", mp3File.getID3v1Tag());
+					ID3v1 tag = mp3File.getID3v1Tag();
+					
+					tag.getGenre();
+					
+					tag.getArtist();
+					tag.getSongTitle();
+					
+					tag.getAlbumTitle();
+					tag.getTrackNumberOnAlbum();
+					
+					tag.getYearReleased();
+					
+
+				}
+
+				if (mp3File.hasID3v2Tag()) {
+					log.debug("ID3v2Tag:{}", mp3File.getID3v2Tag());
+				}
+
+				if (mp3File.hasLyrics3Tag()) {
+					log.debug("Lyrics3Tag:{}", mp3File.getLyrics3Tag());
+				}
+				
 			}
 		}
 		
--- src/main/java/io/hexaforce/songs/model/MusicItem.java
+++ src/main/java/io/hexaforce/songs/model/MusicItem.java
@@ -1,8 +1,13 @@
 package io.hexaforce.songs.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "music_item")
 public class MusicItem extends BaseEntity {
 	private String songTitle;
 	private String albumTitle;
